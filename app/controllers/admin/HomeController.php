1fa31c0312c7a9922ff6e2e1262a1d7f4a71e61b
crynobone <crynobone@gmail.com>
2013-06-03 20:00:22 +0800

    Basic resources.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ app/controllers/admin/HomeController.php
@@ -0,0 +1,9 @@
+<?php 
+
+class AdminHomeController extends BaseController {
+
+	public function getIndex()
+	{
+		return '<h1>We are in Playground</h1>';
+	}
+}
--- app/orchestra.php
+++ app/orchestra.php
@@ -1,3 +1,11 @@
 <?php
 
 Orchestra\Acl::make('playground')->attach(Orchestra\App::memory());
+
+Event::listen('orchestra.started: admin', function ()
+{
+	$playground = Orchestra\Resources::make('playground', [
+		'name' => 'Playground',
+		'uses' => 'restful:AdminHomeController',
+	]);
+});
