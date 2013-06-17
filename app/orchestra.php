5ea67a3f672783edfd5a840220b0d07251cfd614
crynobone <crynobone@gmail.com>
2013-06-17 11:11:37 +0800

    Fixed visible option typo.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- app/orchestra.php
+++ app/orchestra.php
@@ -5,9 +5,9 @@ Orchestra\Acl::make('playground')->attach(Orchestra\App::memory());
 Event::listen('orchestra.started: admin', function ()
 {
 	$playground = Orchestra\Resources::make('playground', [
-		'name'       => 'Playground',
-		'uses'       => 'restful:AdminHomeController',
-		'visibility' => function ()
+		'name'    => 'Playground',
+		'uses'    => 'restful:AdminHomeController',
+		'visible' => function ()
 		{
 			$acl = Orchestra\Acl::make('playground');
 			
